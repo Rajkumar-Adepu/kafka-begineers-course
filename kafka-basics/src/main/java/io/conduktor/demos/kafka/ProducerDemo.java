f43f96a1470e55db5ac5101515093c2bdbd5f36f
Rajkumar-Adepu <foradepurajkumar@gmail.com>
2024-09-07 20:04:55 -0500

    Kafka

---
+++ kafka-basics/src/main/java/io/conduktor/demos/kafka/ProducerDemo.java
@@ -0,0 +1,15 @@
+package io.conduktor.demos.kafka;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ProducerDemo {
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemo.class.getSimpleName());
+
+    public static void main(String[] args) {
+
+        log.info("Hello_world !!");
+
+
+
+    }
+}
